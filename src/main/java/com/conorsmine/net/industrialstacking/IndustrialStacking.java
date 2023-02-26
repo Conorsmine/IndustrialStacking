5f58017ce961240d533aab350a4736e83cd40366
Conorsmine <79007923+Conorsmine@users.noreply.github.com>
2023-02-26 09:49:52 +0100

    Initial commit

---
+++ src/main/java/com/conorsmine/net/industrialstacking/IndustrialStacking.java
@@ -0,0 +1,17 @@
+package com.conorsmine.net.industrialstacking;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class IndustrialStacking extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
